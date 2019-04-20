1d05b468f222a2b7c949360a33a466b909648597
naren <naren@DESKTOP-53TSVQN>
2019-04-20 23:24:21 +0530

    bug fixed related to page size

--- demo-1/src/main/java/com/spring/controller/UserController.java
+++ demo-1/src/main/java/com/spring/controller/UserController.java
@@ -5,12 +5,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.entity.User;
 import com.spring.service.PageNationService;
 
-@Controller
+@RestController
 
 public class UserController {
 
@@ -18,29 +20,25 @@ public class UserController {
 	public PageNationService service;
 
 	@RequestMapping(value = "/users")
+	@ResponseBody
 	public ModelAndView listUser(@RequestParam(value = "pageNo", defaultValue = "0") int pageNo,
 			@RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
 			@RequestParam(value = "sortedColumn", defaultValue = "id") String sortedColumn,
-			@RequestParam(value = "searchBy", defaultValue = "4") int searchBy) {
+			@RequestParam(value = "searchBy", defaultValue = "5") int searchBy) {
 
 		ModelAndView mv = null;
 		Page<User> pagenation = service.pagenation(pageNo, pageSize, sortedColumn, searchBy);
-		
-		
+
 		mv = new ModelAndView();
 		mv.addObject("users", pagenation.getContent());
 		mv.addObject("totalNumberOfPages", pagenation.getTotalPages());
 		mv.addObject("pagesize", pagenation.getNumberOfElements());
 		mv.addObject("pagenumber", pagenation.getNumber());
 		mv.addObject("pageSize", pageSize);
-		mv.addObject("totalrecords",pagenation.getTotalPages()*pagenation.getNumberOfElements());
-		
-		
+		mv.addObject("totalrecords", pagenation.getTotalPages() * pagenation.getNumberOfElements());
+
 		mv.setViewName("usersList");
-		
-		
-		
-		
+
 		return mv;
 	}
 
